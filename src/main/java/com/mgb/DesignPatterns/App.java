6075deb29ff30ce267d190e44a7e0c63b47ad2c1
MOUNESH BADIGER Badiger <moubad53@in.ibm.com>
2019-09-01 11:52:27 +0530

    adding all

---
+++ src/main/java/com/mgb/DesignPatterns/App.java
@@ -0,0 +1,13 @@
+package com.mgb.DesignPatterns;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
